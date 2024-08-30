a8a39065ec5c22094814d0c5c3a92a623fa44767
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-08-30 15:11:22 +0530

    react-icons

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
 function Form() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -8,11 +9,29 @@ function Form() {
     setUsername(event.target.value);
   };
 
+  const handleSubmit = (e) => {
+    //NOTE calling the e.preventDefault() to stop the default behviour of the form => that when we click on the submit button it refreshes the page/
+    e.preventDefault();
+    const data = {
+      username: username,
+      password: password,
+    };
+    console.log(data);
+  };
+
   return (
     <div className="flex gap-10 justify-center">
-      <form className="flex w-96 flex-col p-8 shadow-lg bg-slate-100 rounded-md ">
-        <h1 className='text-center text-3xl font-bold text-blue-500'>Login</h1>
-        <label className="text-lg font-semibold">Username</label>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-96 flex-col p-8 shadow-lg bg-slate-100 rounded-md "
+      >
+        <h1 className="text-center text-3xl font-bold text-blue-500">Login</h1>
+        <div className="flex items-center gap-2">
+          {' '}
+          <FaUser />
+          <label className="text-lg font-semibold">Username</label>
+        </div>
+
         <input
           className="w-full h-10 border border-slate-700 p-2 rounded-md"
           onChange={handleUsernameChange}
